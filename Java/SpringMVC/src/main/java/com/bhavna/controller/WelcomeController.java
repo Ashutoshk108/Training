0571a836c1cb28065bd2da5bc3c93fed9a8a6226
Ashutoshk108 <Ashutosh.Mishra@bhavnacorp.com>
2022-10-07 09:24:34 +0530

    Task

---
+++ Java/SpringMVC/src/main/java/com/bhavna/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.bhavna.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value="/welcome", method= RequestMethod.GET)
+	public String display(){
+		System.out.println("Hello");
+		return "welcome1";
+	}
+}
